3e047fe2f455b0934e45ff343a007b53bd8afd5d
zhd <2929616615@qq.com>
2022-03-30 17:33:36 +0800

    补充表单下载文件功能

--- TOOL/download.js
+++ TOOL/download.js
@@ -7,5 +7,22 @@ let fileTool = {
         document.body.appendChild(a);
         a.click();
         document.body.removeChild(a);
-    }
+    },
+    // form表单提交下载文件
+    download_by_form : function(url,name){
+        // 创建表单
+        const form = document.createElement("form");
+        form.action = url;
+        form.method = "get";
+        form.style.display = "none";
+        // 创建input,用以传参
+        const formItem = document.createElement("input");
+        formItem.value = name;
+        formItem.name = 'name';
+        // 插入到网页中
+        form.appendChild(formItem);
+        document.body.appendChild(form);
+        form.submit();
+        document.body.removeChild(form);
+    } 
 }
